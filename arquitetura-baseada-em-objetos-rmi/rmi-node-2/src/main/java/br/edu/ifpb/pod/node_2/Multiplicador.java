24c3e939aad4733270778024775b4a1dbb361d9b
Michelle Oliveira <miolivc@outlook.com>
2017-09-22 16:37:16 -0300

    questao 01

---
+++ arquitetura-baseada-em-objetos-rmi/rmi-node-2/src/main/java/br/edu/ifpb/pod/node_2/Multiplicador.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.ifpb.pod.node_2;
+
+import br.edu.ifpb.pod.rmi.shared.Calculadora;
+import br.edu.ifpb.pod.rmi.shared.Provider;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+
+/**
+ *
+ * @author miolivc
+ */
+public class Multiplicador extends UnicastRemoteObject implements Calculadora {
+
+    public Multiplicador() throws RemoteException {};
+    
+    @Override
+    public double multt(double x, double y) throws RemoteException {
+        return 2 * y * x;
+    }
+
+    @Override
+    public double divv(double x, double y) throws RemoteException {
+        throw new RemoteException("Esse servidor nao suporta essa operaçao! ");
+//        Esse trecho de codigo esta comentado pq nao tem 
+//        interaçao com o node, esse estoura erro na arquitetura
+
+//        Calculadora calculadora = null;
+//        try {
+//            calculadora = new Provider().getCalculadora("node_3");
+//        } catch (NotBoundException ex) {
+//            ex.printStackTrace();
+//        }
+//        return calculadora.divv(x, y);
+    }
+    
+}
